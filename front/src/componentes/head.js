d95467f92179f56f1da6661592a8bce1bde4c286
Ca8allero02 <ic.caballero06@ciaf.edu.co>
2024-11-28 02:51:54 -0500

    cambios menores
    me sale un bug en el que no se muestra la vista del head y me estresa, ando probando cosas hasta solucionarlo

--- front/src/componentes/head.js
+++ front/src/componentes/head.js
@@ -3,28 +3,13 @@ import React from 'react';
 import './head.css';
 import logo from './assets/images/icon.png';
 
-const Head = () => {
+const HomePage = () => {
     return (
-        <div className="homepage">
-            {/* Logo o imagen de encabezado */}
-            <header className="header">
-                <img src={logo} alt="Logo" className="logo" />
-                <h1>Bienvenidos a Mi Sitio Web</h1>
-            </header>
-
-            {/* Barra de búsqueda */}
-            <div className="search-bar">
-                <input type="text" placeholder="Buscar..." className="search-input" />
-                <button className="search-button">Buscar</button>
-            </div>
-
-            {/* Contenido adicional */}
-            <div className="content">
-                <p>Explora nuestro contenido y descubre todo lo que tenemos para ofrecer.</p>
-                <button className="explore-button">Explorar</button>
-            </div>
-        </div>
+        <header className="header">
+            <img src={logo} alt="Logo" className="logo" />
+            <h1 className="title">Bienvenidos a Mi Sitio Web</h1>
+        </header>
     );
 };
 
-export default Head;
+export default HomePage;
